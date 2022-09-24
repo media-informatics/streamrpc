d489fed0027ca6e66c51f960df916e1cbd5cc10a
Jürgen Singer <jsinger@hs-harz.de>
2022-09-24 10:16:37 +0200

    moved server client, added localhost

---
+++ client/client.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"time"
+
+	"github.com/media-informatics/streamrpc/service"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("cannot connect with server %v", err)
+	}
+	defer conn.Close()
+
+	temperature := service.NewTemperatureServiceClient(conn)
+	const repeat = 10
+	count := &service.Request{Repeat: repeat}
+
+	ctxt, cancel := context.WithTimeout(context.Background(), 2*repeat*time.Second)
+	defer cancel()
+
+	stream, err := temperature.Subscribe(ctxt, count)
+	if err != nil {
+		log.Fatalf("open stream error %v", err)
+	}
+
+	log.Printf("subscription start")
+	for {
+		resp, err := stream.Recv()
+		if err == io.EOF {
+			log.Printf("subscription ended")
+			break
+		}
+		if err != nil {
+			log.Fatalf("cannot receive %v", err)
+		}
+		tz, err := time.LoadLocation("Local")
+		if err != nil {
+			log.Printf("invalid time format %v", err)
+		}
+		time := resp.GetTime().AsTime().In(tz).Format("15:04:05")
+		fmt.Printf("Response received: %v: %v\n", time, resp.GetTemperature())
+	}
+}
---
+++ server/server.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"time"
+
+	"github.com/media-informatics/streamrpc/service"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type TemperatureServiceServer struct {
+	service.UnimplementedTemperatureServiceServer
+}
+
+func (s *TemperatureServiceServer) Subscribe(in *service.Request, srv service.TemperatureService_SubscribeServer) error {
+	count := int(in.GetRepeat())
+	tick := time.Tick(time.Second)
+	ctxt := srv.Context()
+	var err error = nil
+	log.Printf("subscribed - count = %d", count)
+	i := 0
+	for i < count {
+		select {
+		case <-ctxt.Done():
+			err = fmt.Errorf("temperature subscription aborted: %w", ctxt.Err())
+		case <-tick:
+			i++
+			tstamp := timestamppb.Now()
+			temperature := float32(rand.NormFloat64()*2.0 + 28.0)
+			resp := service.Response{
+				Time:        tstamp,
+				Temperature: temperature,
+			}
+			if err = srv.Send(&resp); err != nil {
+				err = fmt.Errorf("send error %w", err)
+			}
+		}
+		if err != nil {
+			log.Printf("server info: %v", err)
+			break
+		}
+	}
+	log.Printf("finished with %d Temperatures", i)
+	return err
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:50000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	server := &TemperatureServiceServer{}
+	service.RegisterTemperatureServiceServer(s, server)
+
+	log.Println("start server")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
