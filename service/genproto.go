e71f0ecdff37bd75ad53805c514ec7155cd7f9b9
Jürgen Singer <jsinger@hs-harz.de>
2022-09-25 08:00:42 +0200

    added comment

--- service/genproto.go
+++ service/genproto.go
@@ -1,5 +1,7 @@
 package service
 
 //go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative temperature.proto
+
+// server location:
 const Port = 50000
 const Host = "localhost"
